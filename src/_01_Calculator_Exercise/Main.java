a5658eb119b2c6e551622c05d80ac1aa720e4cad
Steven <steve@littlecreekdesign.com>
2016-09-29 11:56:13 -0400

    first commit

---
+++ src/_01_Calculator_Exercise/Main.java
@@ -0,0 +1,74 @@
+package _01_Calculator_Exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Calc calc = new Calc();
+		double a = 0;
+		double b = 0;
+		String sign = "";
+		double result = 0;
+		
+		System.out.println("Welcome to My Calculator");
+		
+		//BufferedReader - Reads text from a character-input stream, buffering characters so as to provide for the efficient reading of characters, arrays, and lines
+		
+		//InputStreamReader - A bridge from byte streams to character streams: It reads bytes and decodes them into characters using a specified charset.
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
+		
+		//"Try" and "catch" are represent the handling of exceptions due to data or coding errors during program execution. 
+		//A try block is the block of code in which exceptions occur. A catch block catches and handles try block exceptions.
+		
+		try {
+			System.out.println("Please enter first number, then press Return:");
+			a = Double.parseDouble(reader.readLine());
+			System.out.println("Please enter second number, then press Return:");
+			b = Double.parseDouble(reader.readLine());
+			System.out.println("Please enter the calculation type desired:");
+			System.out.println("Options: +(add) or(subtract) - or *(multiply) or /(divide), then press Return.");
+			sign = reader.readLine();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Switch - Alternate to "if/else". Can be used with byte, short, char, int, enum, String and a few others. 
+		//In this case String = sign. Must have "break" at end of case.
+		
+		switch(sign){
+		case "+":
+			result = calc.add(a, b);
+			System.out.println("Result: ");
+			System.out.println(result);
+			break;
+		case "-":
+			result = calc.subtract(a,b);
+			System.out.println("Result: ");
+			System.out.println(result);
+			break;
+		case "*":
+			result = calc.multiply(a, b);
+			System.out.println("Result: ");
+			System.out.println(result);
+			break;
+		case "/":
+			result = calc.divide(a, b);
+			System.out.println("Result: ");
+			System.out.println(result);
+			break;
+		default:
+			System.out.println("Wrong sign!");
+		
+		
+		}
+		
+	}
+
+}
